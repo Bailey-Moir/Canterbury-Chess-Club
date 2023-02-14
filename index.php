a5f57dba8adeb660bfd102a9126578b3b58af9ee
Bailey Moir <bailey.p.moir@gmail.com>
2023-02-14 21:37:27 +1300

    inital commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "php/header.php"; ?>
+	<title>Canterbury Chess Club</title>
+</head>
+<body>
+	<?php include "php/navbar.php"; ?>
+</body>
+</html>
---
+++ php/navbar.php
@@ -0,0 +1,7 @@
+<div id="nav">
+    <h1><a href="#">Canterbury Chess Club</a></h1>
+    <div id="buttons">
+        <a href="#" class="btn">Home</a>
+        <a href="#" class="btn">Tournaments</a>
+    </div>
+</div>
