943702420008a9ce2ef3d666eb8f2be0e59e430f
Bailey Moir <bailey.p.moir@gmail.com>
2023-02-25 22:16:10 +1300

    chore: basic to-be-improved module system

---
+++ php/modules/board/board.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="./board.css">
+<div class="board white-view">
+    <?php    
+    $piece_order = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'];
+    $alphabet = range('a', 'z');
+    
+    for ($i = 8; $i > 0; $i--) {
+        echo "<div class=\"rank rank-$i\">";
+        for ($j = 0; $j < 8; $j++) {
+            echo "<div class=\"square $alphabet[$j]$i\">";
+            
+            $piece = null;
+                if ($i == 1 || $i == 8) $piece = $piece_order[$j];
+            elseif ($i == 2 || $i == 7) $piece = "P";
+            
+            if ($piece) echo "<div class=\"piece ".( $i > 4 ? "black" : "white" )." $piece\"></div>";
+
+            echo "</div>";
+        }
+        echo "</div>";
+    }
+    ?>
+</div>
