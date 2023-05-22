e77491e72a34a5664a084b4b67822d82aa0087c8
GavithDharmasena <78179011+GavithDharmasena@users.noreply.github.com>
2023-05-23 11:40:46 +1200

    began addition of icons
    Added icons to navbar and footer, footer icons still need work.

--- php/modules/footer/footer.php
+++ php/modules/footer/footer.php
@@ -1,9 +1,8 @@
 <link rel="stylesheet" href="/php/modules/footer/footer.css">
 <div id="foot">
     <div id="buttons">
-        <a href="/php/location.php" class="btn"><i></i><b>Location</b></a>
-        <a href="/php/contact.php" class="btn"><i></i><b>Contact</b></a>
-        <a href="/php/history.php" class="btn"><i></i><b>History</b></a>
+        <a href="/php/location.php" class="btn"><img src="/res/location.svg" alt="Icon"><b>Location</b></a>
+        <a href="/php/contact.php" class="btn"><img src="/res/mail.svg" alt="Icon"><b>Contact</b></a>
+        <a href="/php/history.php" class="btn"><img src="/res/history.svg" alt="Icon"><b>History</b></a>
     </div>
-    <i></i>
 </div>
--- php/modules/navbar/navbar.php
+++ php/modules/navbar/navbar.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" href="/php/modules/navbar/navbar.css">
 <div id="nav">
     <div id="buttons_left">   
-        <a href="index.php"><img src="/res/white_icon.svg" alt="Icon"></a>
+        <a href="index.php"><img src="/res/white_logo.svg" alt="Icon"></a>
         <a href="/php/tournaments.php" class="btn"><i></i><b>Tournaments</b></a>
     </div>
 
     <div id="buttons_right">   
-        <a href="#" class="btn"><i></i><b>Search</b></a>
-        <a href="https://www.facebook.com/CanterburyChessClub/" class="btn"><i></i><b>Facebook</b></a>
-        <a href="/php/login.php" class="btn"><i></i><b>Profile</b></a>
+        <a href="#" class="btn"><img src="/res/search.svg" alt="Icon"><b>Search</b></a>
+        <a href="https://www.facebook.com/CanterburyChessClub/" class="btn"><img src="/res/person_square.svg" alt="Icon"><b>Facebook</b></a>
+        <a href="/php/login.php" class="btn"><img src="/res/person_circle.svg" alt="Icon"><b>Profile</b></a>
     </div>
 </div>
