10fbf92d719ccbd3885b99597b421e993f8cbe5d
Bailey Moir <bailey.p.moir@gmail.com>
2023-08-18 16:27:13 +1200

    merged into about

--- src/header.php
+++ src/header.php
@@ -7,3 +7,4 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <link rel="stylesheet" href="/styles.css">
+<script src="/src/main.js"></script>
--- src/pages/contact/contact.php
+++
@@ -1,5 +0,0 @@
-<h2>Contact</h2>
-<p>E-mail: Canterbury@chess.org.nz</p>
-<p>Facebook: http://www.facebook.com/CanterburyChessClub</p>
-<p>Mobile: 021 1289543</p>
-<img src="" alt="">
