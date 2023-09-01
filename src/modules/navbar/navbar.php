80e348c028f9fdef8ff220886035a8180ed47e2e
Bailey Moir <bailey.p.moir@gmail.com>
2023-09-01 17:16:22 +1200

    redid navbar

--- src/modules/navbar/navbar.php
+++ src/modules/navbar/navbar.php
@@ -1,38 +1,16 @@
 <link rel="stylesheet" href="/src/modules/navbar/navbar.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-<nav class="navbar navbar-expand-lg bg-body-nav">
+<div class="mnav">
+        <a class="mnav-icon-cntr" href="/"><img src="/res/white_logo.svg" alt="Icon"></a>
 
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/"><img src="/res/white_logo.svg" alt="Icon"></a>
+        <a class="mnav-btn" aria-current="page" href="/tournaments"><b>Tournaments</b></a>
+        <a class="mnav-btn" aria-current="page" href="/calendar"><b>Calendar</b></a>
+        <a class="mnav-btn" aria-current="page" href="/coaching"><b>Coaching</b></a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="/tournaments"><b>Tournaments</b></a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="/calendar"><b>Calendar</b></a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="/coaching"><b>Coaching</b></a>
-                </li>
-            </ul>
-
-            <form class="d-flex" role="search">
-                <button class="btn text-white" type="submit"><span class="material-symbols-outlined align-middle">search</span><b>Search</b></button>  
-            </form>
-
-            <a href="https://www.facebook.com/CanterburyChessClub/"><button class="btn text-white" type="submit"><span class="material-symbols-outlined align-middle">account_circle</span><b>facebook</b></button></a>
-
-            <a href="/signin"><button class="btn text-white" type="submit"><span class="material-symbols-outlined align-middle">account_circle</span><b>Profile</b></button></a>
-
+        <div class="right">
+            <a class="mnav-btn" href="https://www.facebook.com/CanterburyChessClub/"><span class="material-symbols-outlined">account_circle</span><b>Facebook</b></a>
+            <a class="mnav-btn" href="/signin"><span class="material-symbols-outlined">account_circle</span><b>Profile</b></a>
         </div>
-    </div>
-</nav>
+</div>
