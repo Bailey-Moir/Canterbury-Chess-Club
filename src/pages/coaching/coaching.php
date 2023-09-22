7cbeb0b9c18229406ac97b7bbbd5c6b662c082da
Bailey Moir <bailey.p.moir@gmail.com>
2023-09-22 21:42:27 +1200

    coaching and responsive game

--- src/pages/coaching/coaching.php
+++ src/pages/coaching/coaching.php
@@ -1,31 +1,33 @@
-<h2>Coaching</h2>
+<h1>Coaching</h1>
 
-<div class="row justify-content-around">
-	Canterbury Chess Club provides coaching courses with experienced coaches. These courses are held at our clubrooms. We have a structured curriculum designed by <a href="https://ratings.fide.com/profile/4301030" style="none;">Craig Hall</a>, an experienced arbiter (referee/umpire) and trainer accredited by the World Chess Federation (FIDE) that we plan our lessons around. There will also be time for practice games at the end. We offer courses for children aged 8-15 on Mondays from 7pm and Fridays from 5:30 to 6:45pm and 7 to 8:15pm during school terms. Note that lessons are not held on the first or last day of term or public holidays (so courses are usually 8-9 lessons).
-</div>
+Canterbury Chess Club provides coaching courses with experienced coaches. These courses are held at our clubrooms. We have a structured curriculum designed by <a href="https://ratings.fide.com/profile/4301030" style="none;">Craig Hall</a>, an experienced arbiter (referee/umpire) and trainer accredited by the World Chess Federation (FIDE) that we plan our lessons around. There will also be time for practice games at the end. We offer courses for children aged 8-15 on Mondays from 7pm and Fridays from 5:30 to 6:45pm and 7 to 8:15pm during school terms. Note that lessons are not held on the first or last day of term or public holidays (so courses are usually 8-9 lessons).
 
-<div class="row justify-content-around">
-	<h3 style="padding-top: 20px;">Monday 7pm Courses</h3>
+
+<h3 class="mt-4">Monday 7pm Courses</h3>
+<ul class="list">
 	<li>Term 3, beginner-intermediate players (basic knowledge would be helpful e.g. knowing how the pieces move, check and checkmate): 24 July to 18 September (inclusive), $80 - <a href="https://docs.google.com/forms/d/e/1FAIpQLSenkm_kNhUBgOtwPHXM2SlRDTDF046iGaXu4smZNV8WgPfdgg/viewform">Sign-up form</a></li>
 	<li>Term 4, intermediate players, building on term 3: 16 October to 11 December (inclusive), $80</li>
+</ul>
 
-	<br>
-
-	<h3 style="padding-top: 20px;">Friday 5:30pm Courses</h3>
+<h3 class="mt-4">Friday 5:30pm Courses</h3>
+<ul class="list">
 	<li>Term 3, beginner-intermediate players (basic knowledge would be helpful e.g. knowing how the pieces move, check and checkmate): 21 July to 15 September (inclusive), $80 - <a href="https://docs.google.com/forms/d/e/1FAIpQLSenkm_kNhUBgOtwPHXM2SlRDTDF046iGaXu4smZNV8WgPfdgg/viewform">Sign-up form</a></li>
 	<li>Term 4, intermediate players, building on term 3: 13 October to 8 December (inclusive), $80</li>
+</ul class="list">
 
-	
-	<h3 style="padding-top: 20px;">Friday 7pm Courses</h3>
+<h3 class="mt-4">Friday 7pm Courses</h3>
+<ul class="list">
 	<li>Term 3, intermediate-advanced players (follows on from terms 1 and 2 - must know rules of chess, basic tactics and checkmates): 21 July to 15 September (inclusive), $80 - sorry, this class is now full.</li>
 	<li>Term 4, more advanced players (will follow on from terms 1-3): 13 October to 8 December (inclusive), $80</li>
+</ul class="list">
 
-	<h3 style="color: red; padding-top: 20px;">Please note that students under 13 must be accompanied by a parent/caregiver for the evening.</h3>
+<b class="p mt-4">Please note that students under 13 must be accompanied by a parent/caregiver for the evening.</b>
+
+<p class="p">We also offer occasional shorter courses on various topics for students aged 11+. Details of these will be added here and posted to our <a href="https://www.facebook.com/CanterburyChessClub/">Facebook Page</a> - please check back here occasionally or follow us there.</p>
 	
-	<p style="padding-bottom: 10px;">We also offer occasional shorter courses on various topics for students aged 11+. Details of these will be added here and posted to our <a href="https://www.facebook.com/CanterburyChessClub/">Facebook Page</a> - please check back here occasionally or follow us there.</p>
-		
-	<p> There is a Minimum number of 4 students, maximum of 12 students per course. We provide all equipment.</p>
-	<p>For queries, please contact Craig Hall using our email or phone number below:</p>
-	<p>canterbury@chess.org.nz</p>
-	<p>021 1289543</p>
-</div>
+<p class="p"> 
+	There is a Minimum number of 4 students, maximum of 12 students per course. We provide all equipment.
+	For queries, please contact Craig Hall using our email or phone number below:
+</p>
+<a class="p" href="mailto:canterbury@chess.org.nz">canterbury@chess.org.nz</a> <br/>
+<a class="p" href="tel:+64211289543">021 1289543</a>
